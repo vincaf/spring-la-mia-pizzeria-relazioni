8c77a7374bf2ded317962386ab4b04dadebf1bd5
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-12 15:57:35 +0100

    feat(PromotionService): add method to find all pizza on promotion list
    - add transactional annotation
    - lazily load objects/collections, we do Hibernate.initialize(Object obj) so that object that is passed as an argument to initialize() method is initialized and can be used outside of the scope of the session

--- Pizzeria/src/main/java/org/generation/italy/demo/service/PromotionService.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/service/PromotionService.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import org.generation.italy.demo.pojo.Promotion;
 import org.generation.italy.demo.repo.PromotionRepo;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class PromotionService {
@@ -28,5 +31,16 @@ public class PromotionService {
 
 	public void deletePromotionById(int id) {
 		promotionRepo.deleteById(id);
+	}
+	
+	@Transactional
+	public List<Promotion> findAllWPizza() {
+		List<Promotion> promotions = promotionRepo.findAll();
+
+		for (Promotion promotion : promotions) {
+			Hibernate.initialize(promotion.getPizzas());
+		}
+
+		return promotions;
 	}
 }

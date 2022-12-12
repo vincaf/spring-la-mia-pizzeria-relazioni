02413dc3cf84c77686a69a18f79906713251b982
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-12 16:11:51 +0100

    feat(PizzeriaApplication): test new object promotion
    - create promotion and save
    - add promotion in the pizza constructor
    - delete promotion by id
    - print the pizzas list and the promotions list

--- Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
@@ -1,11 +1,14 @@
 package org.generation.italy.demo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.pojo.Promotion;
 import org.generation.italy.demo.service.DrinkService;
 import org.generation.italy.demo.service.PizzaService;
+import org.generation.italy.demo.service.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +23,9 @@ public class PizzeriaApplication implements CommandLineRunner {
 	@Autowired
 	private DrinkService drinkService;
 	
+	@Autowired
+	private PromotionService promotionService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaApplication.class, args);
 	}
@@ -27,9 +33,19 @@ public class PizzeriaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception{
 		
-		Pizza pizza_1 = new Pizza("Margherita", "Troppo buona", 7);
-		Pizza pizza_2 = new Pizza("Marinara", "Ci sta, bel sapore", 6);
-		Pizza pizza_3 = new Pizza("Fritta", "Croccante e buona frittura", 8);
+		// promotions
+		
+		Promotion promotion_1 = new Promotion(LocalDate.parse("2022-12-01"), LocalDate.parse("2022-12-24"), "Sconto Natalizio");
+		Promotion promotion_2 = new Promotion(LocalDate.parse("2023-12-28"), LocalDate.parse("2023-12-31"), "Sconto Fine anno");
+		
+		promotionService.save(promotion_1);
+		promotionService.save(promotion_2);
+		
+		// pizza
+		
+		Pizza pizza_1 = new Pizza("Margherita", "Troppo buona", 7, promotion_1);
+		Pizza pizza_2 = new Pizza("Marinara", "Ci sta, bel sapore", 6, promotion_2);
+		Pizza pizza_3 = new Pizza("Fritta", "Croccante e buona frittura", 8, null);
 		
 		pizzaService.save(pizza_1);
 		pizzaService.save(pizza_2);
@@ -50,5 +66,28 @@ public class PizzeriaApplication implements CommandLineRunner {
 		
 		List<Drink> drinks = drinkService.findAll();
 		System.out.println(drinks);
+		
+		// delete
+		
+		promotionService.deletePromotionById(1);
+		
+		// pizza + promotions
+		
+		System.out.println("\n----------\n");
+		
+		for (Pizza pizza : pizzas) {
+			System.err.println(pizza + "\n" + pizza.getPromotion());
+		}
+
+		System.out.println("\n----------\n");
+		
+		List<Promotion> promotions = promotionService.findAllWPizza();
+
+		for (Promotion promotion : promotions) {
+			System.err.println(promotion);
+			for (Pizza pizza : promotion.getPizzas()) {
+				System.err.println("-----\n" + pizza);
+			}
+		}
 	}
 }

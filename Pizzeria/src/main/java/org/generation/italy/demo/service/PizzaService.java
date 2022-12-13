96dba61e3a5336c90f0a2a96c8ad6023aaf57d89
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-13 17:21:58 +0100

    feat(PizzaService): add method find all with ingredients
    add transactional annotation and lazily load collections

--- Pizzeria/src/main/java/org/generation/italy/demo/service/PizzaService.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/service/PizzaService.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import org.generation.italy.demo.pojo.Pizza;
 import org.generation.italy.demo.repo.PizzaRepo;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class PizzaService {
@@ -42,5 +45,16 @@ public class PizzaService {
 	public List<Pizza> findByNome(String nome){
 		
 		return pizzaRepo.findByNomeContainingIgnoreCase(nome);
+	}
+	
+	@Transactional
+	public List<Pizza> findAllWIngredient() {
+		List<Pizza> pizzas = pizzaRepo.findAll();
+
+		for (Pizza p : pizzas) {
+			Hibernate.initialize(p.getIngredients());
+		}
+
+		return pizzas;
 	}
 }

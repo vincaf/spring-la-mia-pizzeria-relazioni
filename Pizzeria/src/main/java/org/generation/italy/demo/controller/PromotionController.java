22ede6e5acd9cf2ff202898e599df7d9e45c891a
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-12-13 11:52:08 +0100

    feat(PromotionController): add GetMapping create promotion
    add methods to create and store a new promotion

--- Pizzeria/src/main/java/org/generation/italy/demo/controller/PromotionController.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/controller/PromotionController.java
@@ -2,13 +2,18 @@ package org.generation.italy.demo.controller;
 
 import java.util.List;
 
+import org.generation.italy.demo.pojo.Pizza;
 import org.generation.italy.demo.pojo.Promotion;
+import org.generation.italy.demo.service.PizzaService;
 import org.generation.italy.demo.service.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/promotion")
@@ -16,6 +21,9 @@ public class PromotionController {
 
 	@Autowired
 	private PromotionService promotionService;
+	
+	@Autowired
+	private PizzaService pizzaService;
 
 	@GetMapping
 	public String index(Model model) {
@@ -23,5 +31,27 @@ public class PromotionController {
 		List<Promotion> promotions = promotionService.findAll();
 		model.addAttribute("promotions", promotions);
 		return "promotions";
+	}
+	
+	@GetMapping("/create")
+	public String getPromotionCreate(Model model) {
+
+		Promotion promotion = new Promotion();
+		List<Pizza> pizzas = pizzaService.findAll();
+		model.addAttribute("promotion", promotion);
+		model.addAttribute("pizzas", pizzas);
+
+		return "promotion-create";
+	}
+
+	@PostMapping("/create")
+	public String storePromotion(@Valid Promotion promotion) {
+		List<Pizza> promotionPizzas = promotion.getPizzas();
+		for (Pizza pizza : promotionPizzas) {
+			pizza.setPromotion(promotion);
+		}
+		promotionService.save(promotion);
+
+		return "redirect:/promotion";
 	}
 }
